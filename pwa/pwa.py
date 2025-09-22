8acd851d2dab7eb4386f8b4121e9b8b595742498
sean <dongsean515@gmail.com>
2025-09-22 15:09:30 +0800

    refactor: 清理旧代码并添加新的Tensor处理模块
    删除不再使用的PWA相关代码和配置文件
    新增Tensor模块用于数据处理和动量计算
    添加merge.py和filter.py进行数据合并和筛选
    更新get_P_f.py计算动量并保存结果

--- pwa/pwa.py
+++
@@ -1,7 +0,0 @@
-{% import "pwa/calculate_annex.py" as annex with context%}
-{% extends "pwa/calculate_frame.py" %}
-
-{% block likelihood %}
-    {{annex.data_likelihood()}}
-    {{annex.mc_likelihood()}}
-{% endblock %}
